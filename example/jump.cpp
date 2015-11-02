380f7821ca338812d249d76da36fd67b09b1c82a
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-11-02 20:44:49 +0100

    change example jump

--- example/jump.cpp
+++ example/jump.cpp
@@ -22,15 +22,12 @@ typedef ctx::simple_stack_allocator<
 >       stack_allocator;
 
 void f1( ctx::transfer_t t1) {
-    BOOST_ASSERT( 3 == (int)t1.data);
-    std::cout << "f1: entered first time : " << (int)t1.data << std::endl;
-    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, (void*)5);
-    BOOST_ASSERT( 7 == (int)t2.data);
-    std::cout << "f1: entered second time : " << (int)t2.data << std::endl;
-    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, (void*)9);
-    BOOST_ASSERT( 11 == (int)t3.data);
-    std::cout << "f1: entered third time : " << (int)t3.data << std::endl;
-    ctx::jump_fcontext( t3.fctx, (void*)13);
+    std::cout << "f1: entered first time" << std::endl;
+    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, t1.data);
+    std::cout << "f1: entered second time" << std::endl;
+    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, t2.data);
+    std::cout << "f1: entered third time" << std::endl;
+    ctx::jump_fcontext( t3.fctx, t3.data);
 }
 
 int main( int argc, char * argv[]) {
@@ -40,15 +37,12 @@ int main( int argc, char * argv[]) {
     ctx::fcontext_t ctx = ctx::make_fcontext( base1, stack_allocator::default_stacksize(), f1);
 
     std::cout << "main: call start_fcontext( ctx, 0)" << std::endl;
-    ctx::transfer_t t1 = ctx::jump_fcontext( ctx, (void*)3);
-    BOOST_ASSERT( 5 == (int)t1.data);
-    std::cout << "f1: returned first time : " << (int)t1.data << std::endl;
-    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, (void*)7);
-    BOOST_ASSERT( 9 == (int)t2.data);
-    std::cout << "f1: returned second time : " << (int)t2.data << std::endl;
-    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, (void*)11);
-    BOOST_ASSERT( 13 == (int)t3.data);
-    std::cout << "f1: returned third time : " << (int)t3.data << std::endl;
+    ctx::transfer_t t1 = ctx::jump_fcontext( ctx, 0);
+    std::cout << "f1: returned first time" << std::endl;
+    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, 0);
+    std::cout << "f1: returned second time" << std::endl;
+    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, 0);
+    std::cout << "f1: returned third time" << std::endl;
 
     std::cout << "main: done" << std::endl;
 
