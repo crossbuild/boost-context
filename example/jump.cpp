37bd6c7d7595b257f0ce1161b4a752f90e7c6896
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-11-02 21:01:28 +0100

    use only one transfer_t in example jump

--- example/jump.cpp
+++ example/jump.cpp
@@ -21,27 +21,27 @@ typedef ctx::simple_stack_allocator<
     8 * 1024 // 8kB
 >       stack_allocator;
 
-void f1( ctx::transfer_t t1) {
+void f1( ctx::transfer_t t_) {
     std::cout << "f1: entered first time" << std::endl;
-    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, t1.data);
+    ctx::transfer_t t = ctx::jump_fcontext( t_.fctx, t_.data);
     std::cout << "f1: entered second time" << std::endl;
-    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, t2.data);
+    t = ctx::jump_fcontext( t.fctx, t.data);
     std::cout << "f1: entered third time" << std::endl;
-    ctx::jump_fcontext( t3.fctx, t3.data);
+    ctx::jump_fcontext( t.fctx, t.data);
 }
 
 int main( int argc, char * argv[]) {
     stack_allocator alloc;
 
-    void * base1 = alloc.allocate( stack_allocator::default_stacksize());
-    ctx::fcontext_t ctx = ctx::make_fcontext( base1, stack_allocator::default_stacksize(), f1);
+    void * base = alloc.allocate( stack_allocator::default_stacksize());
+    ctx::fcontext_t ctx = ctx::make_fcontext( base, stack_allocator::default_stacksize(), f1);
 
     std::cout << "main: call start_fcontext( ctx, 0)" << std::endl;
-    ctx::transfer_t t1 = ctx::jump_fcontext( ctx, 0);
+    ctx::transfer_t t = ctx::jump_fcontext( ctx, 0);
     std::cout << "f1: returned first time" << std::endl;
-    ctx::transfer_t t2 = ctx::jump_fcontext( t1.fctx, 0);
+    t = ctx::jump_fcontext( t.fctx, 0);
     std::cout << "f1: returned second time" << std::endl;
-    ctx::transfer_t t3 = ctx::jump_fcontext( t2.fctx, 0);
+    t = ctx::jump_fcontext( t.fctx, 0);
     std::cout << "f1: returned third time" << std::endl;
 
     std::cout << "main: done" << std::endl;
